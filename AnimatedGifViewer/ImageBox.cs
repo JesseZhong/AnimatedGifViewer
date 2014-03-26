cec9538c87dddf553384bdac2236ed081e1e3d65
JesseZhong <jzz119@psu.edu>
2014-03-26 17:56:18 -0400

    Renamed OuterPanel to Window. Added the ability to center the picture box within the window. Separated size check functions to the follow: When the image is larger than the window. When the current picture box size is larger than the window. When the current picture box size is smaller than the window.
    The first one is mutually exclusive of the last two. While the last two share logic overlap between the two.
    
    Picture box size checks are performed during every resize and zoom: whenever the picture box is smaller than the window, the picture box will be centered in the window.

--- AnimatedGifViewer/ImageBox.cs
+++ AnimatedGifViewer/ImageBox.cs
@@ -9,7 +9,7 @@ namespace AnimatedGifViewer {
 
 		#region Members
 		private System.Windows.Forms.PictureBox PictureBox;
-		private System.Windows.Forms.Panel OuterPanel;
+		private System.Windows.Forms.Panel Window;
 		private System.ComponentModel.Container components = null;
 		private bool fitToWindow;
 		#endregion
@@ -34,8 +34,8 @@ namespace AnimatedGifViewer {
 		/// </summary>
 		private void InitializeComponent() {
 			this.PictureBox = new System.Windows.Forms.PictureBox();
-			this.OuterPanel = new System.Windows.Forms.Panel();
-			this.OuterPanel.SuspendLayout();
+			this.Window = new System.Windows.Forms.Panel();
+			this.Window.SuspendLayout();
 			this.SuspendLayout();
 			
 			// PictureBox
@@ -46,41 +46,41 @@ namespace AnimatedGifViewer {
 			this.PictureBox.TabIndex = 3;
 			this.PictureBox.TabStop = false;
 
-			// OuterPanel
-			this.OuterPanel.AutoScroll = false;
-			this.OuterPanel.BackColor = System.Drawing.Color.White;
-			this.OuterPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.OuterPanel.Controls.Add(this.PictureBox);
-			this.OuterPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.OuterPanel.Location = new System.Drawing.Point(0, 0);
-			this.OuterPanel.Name = "OuterPanel";
-			this.OuterPanel.Size = new System.Drawing.Size(this.Width, this.Height);
-			this.OuterPanel.TabIndex = 4;
+			// Window
+			this.Window.AutoScroll = false;
+			this.Window.BackColor = System.Drawing.Color.White;
+			this.Window.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Window.Controls.Add(this.PictureBox);
+			this.Window.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Window.Location = new System.Drawing.Point(0, 0);
+			this.Window.Name = "Window";
+			this.Window.Size = new System.Drawing.Size(this.Width, this.Height);
+			this.Window.TabIndex = 4;
 			
 			// PictureBox
-			this.Controls.Add(this.OuterPanel);
+			this.Controls.Add(this.Window);
 			this.Name = "PictureBox";
 			this.Size = new System.Drawing.Size(210, 190);
-			this.OuterPanel.ResumeLayout(false);
+			this.Window.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 			// Event handlers
-			this.OuterPanel.MouseEnter += new EventHandler(this.ImageBox_MouseEnter);
+			this.Window.MouseEnter += new EventHandler(this.ImageBox_MouseEnter);
 			this.PictureBox.MouseEnter += new EventHandler(this.ImageBox_MouseEnter);
-			this.OuterPanel.MouseWheel += new MouseEventHandler(this.ImageBox_MouseWheel);
-			this.OuterPanel.Resize += new EventHandler(this.OuterPanel_Resize);
+			this.Window.MouseWheel += new MouseEventHandler(this.ImageBox_MouseWheel);
+			this.Window.Resize += new EventHandler(this.OuterPanel_Resize);
 		}
 
 		/// <summary>
 		/// Creates a simple red cross as a bitmap and displays it in the picture box.
 		/// </summary>
 		private void RedCross() {
-			Bitmap bmp = new Bitmap(OuterPanel.Width, OuterPanel.Height, System.Drawing.Imaging.PixelFormat.Format16bppRgb555);
+			Bitmap bmp = new Bitmap(Window.Width, Window.Height, System.Drawing.Imaging.PixelFormat.Format16bppRgb555);
 			Graphics gr;
 			gr = Graphics.FromImage(bmp);
 			Pen pencil = new Pen(Color.Red, 5);
-			gr.DrawLine(pencil, 0, 0, OuterPanel.Width, OuterPanel.Height);
-			gr.DrawLine(pencil, 0, OuterPanel.Height, OuterPanel.Width, 0);
+			gr.DrawLine(pencil, 0, 0, Window.Width, Window.Height);
+			gr.DrawLine(pencil, 0, Window.Height, Window.Width, 0);
 			PictureBox.Image = bmp;
 			gr.Dispose();
 		}
@@ -126,20 +126,49 @@ namespace AnimatedGifViewer {
 		/// </summary>
 		[Browsable(false)]
 		public BorderStyle Border {
-			get { return OuterPanel.BorderStyle; }
-			set { OuterPanel.BorderStyle = value; }
+			get { return Window.BorderStyle; }
+			set { Window.BorderStyle = value; }
 		}
 		#endregion
 
 		#region Zooming Methods
 		/// <summary>
-		/// Checks if the picture box goes out of bounds of the outer panel.
+		/// Checks if the image size exceeds the size of the window.
 		/// </summary>
 		/// <returns>True if it does, false otherwise.</returns>
-		private bool CompareControlSize() {
-			if ((this.OuterPanel.Width < this.PictureBox.Image.Width) ||
-				(this.OuterPanel.Height < this.PictureBox.Image.Height))
-				return true;
+		private bool IsImageLargerThanWindow() {
+			if ((this.Window != null) &&
+				(this.PictureBox != null) &&
+				(this.PictureBox.Image != null))
+					if ((this.Window.Width < this.PictureBox.Image.Width) ||
+						(this.Window.Height < this.PictureBox.Image.Height))
+						return true;
+			return false;
+		}
+
+		/// <summary>
+		/// Checks if the picture box goes out of size of the window.
+		/// </summary>
+		/// <returns>True if it does, false otherwise.</returns>
+		private bool IsPictureLargerThanWindow() {
+			if ((this.Window != null) &&
+				(this.PictureBox != null))
+				if ((this.Window.Width < this.PictureBox.Width) ||
+					(this.Window.Height < this.PictureBox.Height))
+						return true;
+			return false;
+		}
+
+		/// <summary>
+		/// Checks if the picture box falls within the size of the window.
+		/// </summary>
+		/// <returns>True if it does, false otherwise.</returns>
+		private bool IsPictureWithinWindow() {
+			if ((this.Window != null) &&
+				(this.PictureBox != null))
+				if ((this.Window.Width >= this.PictureBox.Width) ||
+					(this.Window.Height >= this.PictureBox.Height))
+					return true;
 			return false;
 		}
 
@@ -148,8 +177,8 @@ namespace AnimatedGifViewer {
 		/// </summary>
 		/// <remarks>Maximum of 5 times larger.</remarks>
 		private void ZoomIn() {
-			if ((this.PictureBox.Width < (MINMAX * this.OuterPanel.Width)) &&
-				(this.PictureBox.Height < (MINMAX * this.OuterPanel.Height))) {
+			if ((this.PictureBox.Width < (MINMAX * this.Window.Width)) &&
+				(this.PictureBox.Height < (MINMAX * this.Window.Height))) {
 				this.PictureBox.Width = Convert.ToInt32(this.PictureBox.Width * ZOOMFACTOR);
 				this.PictureBox.Height = Convert.ToInt32(this.PictureBox.Height * ZOOMFACTOR);
 				this.PictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -161,8 +190,8 @@ namespace AnimatedGifViewer {
 		/// </summary>
 		/// <remarks>Minimum of 5 times smaller.</remarks>
 		private void ZoomOut() {
-			if ((this.PictureBox.Width > (this.OuterPanel.Width / MINMAX)) &&
-				(this.PictureBox.Height > (this.OuterPanel.Height / MINMAX))) {
+			if ((this.PictureBox.Width > (this.Window.Width / MINMAX)) &&
+				(this.PictureBox.Height > (this.Window.Height / MINMAX))) {
 				this.PictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
 				this.PictureBox.Width = Convert.ToInt32(this.PictureBox.Width / ZOOMFACTOR);
 				this.PictureBox.Height = Convert.ToInt32(this.PictureBox.Height / ZOOMFACTOR);
@@ -173,10 +202,15 @@ namespace AnimatedGifViewer {
 		/// If the image is larger than the window, scale it down to fit within the window.
 		/// </summary>
 		private void FitToWindow() {
-			if (this.CompareControlSize()) {
-				double widthRatio = this.PictureBox.Image.Width / (double)this.OuterPanel.Width;
-				double heightRatio = this.PictureBox.Image.Height / (double)this.OuterPanel.Height;
 
+			// Test if the image goes out of the window's bounds.
+			if (this.IsImageLargerThanWindow()) {
+
+				// Calculate image to window ratio for width and height.
+				double widthRatio = this.PictureBox.Image.Width / (double)this.Window.Width;
+				double heightRatio = this.PictureBox.Image.Height / (double)this.Window.Height;
+
+				// Compare which ratio is larger, and size down the picture box accordingly.
 				if (widthRatio > heightRatio) {
 					this.PictureBox.Width = Convert.ToInt32(this.PictureBox.Image.Width / widthRatio);
 					this.PictureBox.Height = Convert.ToInt32(this.PictureBox.Image.Height / widthRatio);
@@ -184,19 +218,34 @@ namespace AnimatedGifViewer {
 					this.PictureBox.Width = Convert.ToInt32(this.PictureBox.Image.Width / heightRatio);
 					this.PictureBox.Height = Convert.ToInt32(this.PictureBox.Image.Height / heightRatio);
 				}
-
-				this.PictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
 			} else {
+				// Resize the picture box to the image's 
+				// dimensions if the dimensions fit inside the window.
 				this.PictureBox.Width = this.PictureBox.Image.Width;
 				this.PictureBox.Height = this.PictureBox.Image.Height;
 			}
+
+			// Make sure the image fills the picture box.
+			this.PictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+			// Center the image in the window.
+			this.CenterImage();
+		}
+
+		/// <summary>
+		/// Centers the image box within the window.
+		/// </summary>
+		private void CenterImage() {
+			this.PictureBox.Location = new System.Drawing.Point(
+				(this.Window.Width - this.PictureBox.Width) / 2, 
+				(this.Window.Height - this.PictureBox.Height) / 2);
 		}
 		#endregion
 
 		#region Mouse Handlers
 		/// <summary>
 		/// Zoom in or out when the mouse wheel is moved.
-		/// If the image is larger than the outer panel,
+		/// If the image is larger than the window,
 		/// panning of the image is enabled for the mouse.
 		/// </summary>
 		/// <param name="sender">ImageBox Controls</param>
@@ -208,10 +257,16 @@ namespace AnimatedGifViewer {
 			else
 				this.ZoomIn();
 
-			if (this.CompareControlSize()) {
+			if (this.IsPictureLargerThanWindow()) {
 				this.PictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
-			} else
+			} else {
 				this.PictureBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+			}
+
+			// Center the picture box if 
+			// it is smaller than the window.
+			if (this.IsPictureWithinWindow())
+				this.CenterImage();
 		}
 
 		/// <summary>
@@ -234,7 +289,7 @@ namespace AnimatedGifViewer {
 		}
 		#endregion
 
-		#region Control Handler
+		#region Control Handlers
 		/// <summary>
 		/// If the image is in "fit to window" state, the
 		/// image will be resized on the control's resize event.
@@ -244,7 +299,24 @@ namespace AnimatedGifViewer {
 		private void OuterPanel_Resize(object sender, EventArgs e) {
 			if (this.fitToWindow)
 				this.FitToWindow();
+			if (this.IsPictureLargerThanWindow()) {
+				if (this.NearZoom != null)
+					this.NearZoom(this, e);
+			}
+
+			// Center the picture box if 
+			// it is smaller than the window.
+			if (this.IsPictureWithinWindow())
+				this.CenterImage();
 		}
+		#endregion
+
+		#region Control Events
+		/// <summary>
+		/// This event is raised when the image extends 
+		/// out of the windows bounds due to zooming.
+		/// </summary>
+		public event EventHandler NearZoom;
 		#endregion
 	}
 }
